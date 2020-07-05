686412fbbe00746a8645d9eebb9c2cc6d325f08f
StrangerealIntel <54320855+StrangerealIntel@users.noreply.github.com>
2020-07-05 22:26:04 +0200

    Create DecodeAlgoMagecart.js

---
+++ 2020-07-05/Magecart/DecodeAlgoMagecart.js
@@ -0,0 +1,94 @@
+var tab=['FMKVNsKI','XjzDrMOK','OWXCpsKJc8Oy','FgQDPlDCgQ==','PkZMDcOTwop1worDiXt3eznCsQ==','w7sxch7DuznDlA==','woZ7X2kAbMKZcTJLDw==','UMOUKcKjTg==','Y1PCosONH8O7','w5UWwp7DiDM=','RWXCg8Ok','wpjCnsOeLQ==','w7l1Hm/DnBbDlkrConlpVcKiBAI=','wps6w7Zgw4taw5nDtUA/wrrDtMKSwoTDoDvDhwrCkmYlwp4sH0PDgsKiwpjDjcKDwqorw4pAf8ONw5BoYXLCjGrDo8OifsKGNMOxIcKEwodGw71IwrlCw73CpBjCgcKhw7F/DQrCm2VvWcKDw7BLbsO2w5pdPcO7woTCm8OtcMOgIcK4w6LDhCZHwrtaU8Oa','woDCoQ3CoQ==','CMOxw6lZwoMcUcK0wpE=','KMOfMwM=','wrYfwpQXcMKCwol/w6wPEMKF','BMOpw6Ru','RUnDoVM=','wp8+w718w4F7w6DCvFA=','wq0Qw5lZ','McOBw4rCsA==','wpkDGcODworDoMOow5XDiMOo','WsOwQMKiLBE=','N8KUwop+w7HDoMO7wp4iMcKgw7cyBQ8=','K8KBasOU','e2kibHow','UMO2UcKlGQ==','LcOMMAJ1VybDn8OKXQhv','V8OtTcK8','b8Ota1pwAw==','D8Krw68=','NcKowqlUwqc=','P8OBw6pfwoIJU8KO','wpETGcOzwqzDq8OJw53Dm8OlXw==','wrJhwpkHw59Z','wrnDjWEQw5oxwrPCssOQ','f8OxU8K1DxDCpQ==','VEPDrEXDn8Ky','wrEUw45Sw4fCiw==','MsO3w7fCkg==','w7FjEnHDmA3DpxjCg2Jp','TTrDiS0cfw==','wr5hwpgCw5NF','wqQIwpcfQ8KCwodjw4o=','IcONMAEbTybDn8O6Vw==','w67CqsKRRA==','w7l1Hm/DnBbDlkrCsH9pXcK1Pg==','F8OSwp3Cpw==','w5wFwpvDnjjDlsOZZ8KvwovCrQ==','wrMMwpcW','I8O2w61kAcOs','DHEXJw==','NsOxwoMD','wqVlwpAaw6lfIA==','LcKYdsOXO8OuUw==','VcO9T8K3GQ0=','wpRvwpgG','w4o+w4VTwoob','d8OtTMKyCBc=','GsOWRWc=','wqAYwogS','w6AWwoTDngbDg8OKcsKr','woM6w7dw','O8OVw5PCs8Kew5o=','HUJMKQ==','OXRsw4A=','VTDDrRknWQ==','w64LCHU0woABVw==','AsOmw7Z4','w7DDiEzDvsKEJQ==','wr7CqcK+w5PCrzDDqiFbw6AZ','V8OXCsKTZMK9','PUZOPMOQwoB0wpw=','YsO3Q8K6CAbCtsO3YcKEHifDuQ==','ccO3TcK0CBfCjsK2VsKRJCPDiQw=','JmfCvXQ=','w6gYwpDDkTPDgcOMM8KAwpHCrcOUw4PDosOfw5XCig==','OWnCq39Gb8KVwoRxG8OAwrxqwodQ','wqA0w7Rhw4Y=','IREFMg==','wqrCnsOGPWcRQQ==','cUnDs37DncKhY8KlwpDDtUMwXCxNS8Otwp0SwoYFOcODwrt5','O8ORwpvCrhPClA==','BWPCvUB3T8Knw5FTAsO0wrtuwoc=','wol6wocKw5RMO8OlEg==','HcOow6Bza3cXWcK9FlTDkMK5YA==','w6fCrsKIdG8MJ8KZdnEVLnvCkcOCwrUqw64=','TTzDssOVIQ==','wqfCvgTCq8KBQVp2wpFLw7JbTcKKwqY=','KcO1w6t8YGACDcKVC0g=','wqgTw5NZ','w4YLC3Qlwps=','w7zDlVnDqcK3Bk4PQsKN','RsOJRmk8FMKxwofDscOJUitLaRA=','DMKBcsOZ','AhUFAmrCpcO9wrLDo8K3JhfCt10YTE/Dkw==','FMOkfkw=','AcOtECE=','BHTCpnhgZMKAw5Z8AcOJwrs=','YMOuFg==','wqnDjGANw5s7wr/CpsOtwq3CrjQ=','wrwIwpUdR8KC','VTzDs8OpM1UDJ1ArHntfNw==','wrMCwo4UR8KYwpFSw4YE','GMKewpJfw7jDt8OQwpI+NsKnw7sqPAsgw5hwHSw=','wrMCwpUJR8KYwp1uw5sPBg==','w6zCo8K/VA==','QsO6ZA==','w5vCpMKeS14sHsOQXGgSPmrCm8OCwrUT','EsObPh0=','XmNrLU3CtgkI','DMKXwo95w7E=','w70QAnMmwoAAXMOx','W8OGwqrCksOxw6guABs2','w6p1EGHDnAc=','w47CvRfDlA==','GRPClHE7','IMO3ZV13Si80PQ==','wqU0w7Zxw4tn','wo3DvGjDn8ObwrQhwpTDtw==','W0nDtGLDucKJecKmwog=','CRfCnGAdw6liRsO7wpE=','CcOga1s=','A8OgZF0=','w6nCpcKYREMADA==','cj7DkS84dsOOwrvDgw==','SnNuMQ==','N8Ogfmx0WzshNsOtwr4YZ8KS','worDi8Op','w6fCrsKIdG8MJ8KZdGgIOG0=','CMOxw6ByJ8OxA8OMw4/Cog==','w4p1DmzDgxrDjB7CkGE=','GcKJwol3w5fDusOcwoUTLcKww5w=','wrxsVXgZW8K6','wp1tXXkEZsKQYjNQM8KhwrAq','w5JdJw3Dtg==','F2PCqMKedcO1w6dqXQ==','w6UbFUknwogaXMOiw7F8XsKdT2xZNWzCmjTDjQ==','PsOkZ0w=','woTDjH4bw5Ek','KB7CjndowrI=','LRvDvsKdWsKrw7Buw7XDkcKcw74tDwczUMKdT8KCw6TCgUcrO8OiYMKdw5w=','BhkFLg==','elnDrkLDjsKpeMKu','w6fCjzTDtsOr','bF7CrcOYKsOn','wqQJw45Ew5bCkcKhMcK9','w4cydhXDqyLDgsKOw4oa','w4VQOhzDssKt','EMK7w6AiXsOFVsKFw7rCpGBFc8O7wpc+NMK9wqZCw47DnVJTawZZYGDCg8Omw5DDhMOHeMKXQBPDh8KKTcOjWsOF','woMuw7Z5','QjfDhjgreMOYwr/Dryo=','wp0OH8O5wrPDhg==','eVfCoMOfDg==','wq4Bw49Dw4zCnsKtOQ==','w4vCqsKqR8OlRcOuHAQF','w4w7ZCjDnyPDtsKZw6Qew7V3dUk=','w5AfwpPDiRXDjcOcdsKHwpA=','w5Q7ZRTDjQ==','OWnCq39Gb8KVwoR5G8ODwr17wpxiwqfDsQ==','w7rDjknDqcK9HkE=','RjrDkw8EcsORwr/DgCoUwq3CgFBaY2vDvQwk','cE3Ds1XDlMKhesKl','w4XCqA/DiMKrwpljKQIVw6jCgcK1SzjDiifDr38+w5tfX8OLwqVWMMKAw6EQQcOvB8OSYcKpw4/DgU3CssOpw6A=','HGjCpcKC','LcOQesOH','D8Oaelt3SjkbBw==','LsOPw5zCu8Kew4vCmcOHwrpmTMK9wqfDsT0=','Lh7CiXM=','w6sfCno=','wrZqUG8gYA==','ScODXk4sFQ==','wq8Fw49ow4XCnsK0OcKvVW3Cm8KsHzoLwojDscKvwo9MJg==','IcKOaMOdO8ONSV82w44=','w4AHwp7DkiI=','wrRvVQ==','L3Nww4VoOsO9QMKAwrt3Jg==','wr5vwoEC','CMO4w61s','wpB9wpoNwpdYJsOxAsOawoVfGcKv','JMKVwpN2w7jCvsOTwoI8LsO4w5cmBAYa','wokPA8OywrHDlA==','w45ZOxnDp8Kp','w7DDgUHDoA==','MmbCuMKN','O3wKJVA=','FMOjw6TDnkQAwpvDt8OHwrPDtcKqw4HDm8OoPMOTwpXDmzHDsA==','TsOPwqfClcOX','JsOgeF1xXTco','w5cLEnB4wrYKXMO2w79hZA==','w6nCgMKIccOjbMONMQw7BERuw7nCmcKgRMK0ECh3wrHDjU1dPzzDtArDu8KcNykKwqsOwp/CgsOjXEHCjMKTw4U3GXXCvzPCgHp9ZcKEw77CgcOGecKqwqfCgMO8wp4Xew==','aUTCo8OHKMO7wqMtwpvCjsOMwrY=','WmMif2Y=','w4TCp8KlUsOSQg==','OsO/wpsCw4Vs','FxUBO1/ChcOV','NcKKw4EwCQ==','Yg7DgxkuBQ==','E38BLFBtwqxUw5Y0w7fDvcKpIQ==','O2QRI1B6woNEw4o=','EMKZwox/w7fDpg==','wrrCk8OFJ3A=','w6PCqRbDmsK9w5E=','OcObw7VH','wq/Ck8ODK2c=','w7FZOxrDl8Kgw5rCug==','wqTDtm3DnMORwrkKw53DkcKFw61uwqLDuMO5w5A=','EmnCumFAY8KFw4E=','NMOGw7hZwq8HWsKOwo8B','wqnDlmYXw4AkwqPCi8OAwqg=','JMKFaMOfPcOy','w5AWwp7Dlw==','J2AGKA==','w7kkwr3DtWU=','w4tSOxvDocKJw4vCsmzDnQs=','IMO1w610TXwCC8K/C0LDlA==','aTDDiy4NZcO6wrPDnC0TwqHCmGle','PW3CrGxweMKT','SnRyLU3CrRwLFQ==','w4FUJxHDvsKk','GMO8w61m','wqVtQmkCe8K4Zg==','QjfDhjgpYw==','RMOdNMKVf8Kwwptrw7zDtjfCksK1','K0ZIJMOewox9','w4FTOw3Dp8Kzw5vCuH/Dmg0=','E8OPw61K','CMObw61Nw5Q3W8KFwq0aIMK3','wpx8wpoOw7lDM8OxKMObwoZT','woXDm3kcw5ci','w7nDk0LDog==','wrvCmsOeHVYdYXPDgMOOVFN+woU=','G8Orw650C8OqHsOZw6s=','wrY0w7h/w4t2w4vDtXIlwofDo8KUwoTDoTLCug==','wrtjXHg=','S8O/fWQ=','wplmwpQRw7lENsOmKsOA','wpIDA8OxwqrDiw==','wplvwpkP','ERUdMk7CjsOfwrXDqg==','Vz7Diz8N','GnVu','C8KUwrVuw6bDu8OTwpA=','wq/CrcKmw7PCng==','SMOcwqTClMOdw7MzFT8=','O8OLw7dMwpgA','FcO+w6LCgXQbwo3Du8OowqA=','ORrCk2Y=','wrMFwpoIcMKFwoxow64U','FcO1woMzw7RdwrHDnMKPRcOM','wo7DmGcY','w6PCqsKQTQ==','BhrCk3Uqw64=','XjXDocOUB1Q3MH4v','JsKPYsOdHcOjUF4=','CGnCoHs=','E8Kewoh9w6DDug==','w7nDk0LDosOoIlMZX8KLwqdjw65A','w47CtBrDisKZw5c=','w4fCoMKhUMOFXg==','GcK5w5ghA0HCoCpf','wovDvGPDk8OEwrIRwpPDtg==','fmdpOA==','eTzDtMOD','w5QvZA8=','w4FyHXfDuAE=','FxUWPlHCiA==','w4wfEnc=','DMKPwpR/w7HDpsOmw4cN','O8KawpJ7','w6DDlF/DpcKrNk4NTw==','JMOqWV1qVzgj','w4wfEnM=','Em59w4JOMcOoEsKQwqBhKsOqwrhx','w5fDgVnDrQ==','OMKSacOMJsOuWUsn','XHRyNGHCsQQJM3ELcQ==','EcO4wpYUw6Fq','w4RzDnbDjRvDgwfClA==','w4RJOx3Dp8Kow4HCtQ==','w7FIJxfDvcKm','wqEBw5Bb','wrxxeHMIYMK1YixNB8Klwrk=','wqjCsBzCqg==','aljCr8OFD8O2','bcONLcKiTsKB','wrIww79sw71hw40=','McOHw6tYwpgGX8KGwqs=','JcKOw4khL1rCqxBBDQ==','woYBw4hW','MMOSwoXCqg==','SMOPwrnCk8OX','woPCtAbCqMKQWw==','K8KJcsOB','HsOxwoQSw45/wpHDlg==','K3gCNHZhwrwRw5Yy','Wk7DjyFqwrMwFMKCw5wHQcK4NWZ0','FMOkfkg=','GWXCpsKJc8Oy','TD0R','wp/CqcO8','JcKUw40yGFDCihllFG3DvsOi','ChIbMl3Ckg==','cyjDrcOEIUk=','wozDtWbDlcOR','D8OfKw4=','V8O5TMK1','EMORPQVTXDPCjcOPRwJpesKCWnjDhg==','wrTCvgrCpcKBUGg/wq1Nw6t3ScKVwp4=','wqVwXmkOYMKlcyU=','DWBrw4k=','CsOeYMORS8KVGCpRDsKT','M8OweV13UzM2HMO4wogA','S8OawrnCicOcw6AjAyM=','KsKVYcKVOsOuUlIsw4FHw6xWfl7CjCVHMVfDrQ==','S8OLwqfChcORw7M=','NMOgfF13UTo3O8Oxwp0PScKT','wo4zw7tnw616w5vCsHUk','wqZwUg==','a1/Cv8OaCsOnwqE3wp3Cl8ONwr1r','AcOkwoUPw455wpXDlcKY','BhgQJX3CicOUwr7DjsKv','GsOzw63ClEMc','w6XDgUHDucKg','wpjCnsOeKQ==','wooDFcOiwr/DkcOJw5U=','OcKSwpR/w7bDp8Oa','cEnDrkbDjsKo','wqFtYmkTfcKyZA==','XsO9VcKFOSbCj8K+S8KQHifDlw==','LmRrw61HN8OxV8KtwqBROsONwrs=','YsONXkY=','O8KawpJ/','wp4YwpYYVsKY','O2R+w4xSAcOoU8K3wrE=','w5/ClMKMU1Q7BcKvRQ==','wr/CnsOGJA==','wqACwogOUMKFwoxo','KcO9w6bCimQAwps=','VmNzPlbCsQ==','IcOiw7ZMWlcrFsKJFlU=','GHLCoMKA','KGV7w5pOIcOvV8Kw','wpXDqmDDmMKZwqkKwo/DusKEw6VrwqHDoA==','EcO5woMf','w4p7D0rDjhvDshjCnn1+TsK4Gg==','U204ag==','K38ONllrwqwR','w64MCXsDwoEPS8OWw79hZA==','QDvDgzgNZMOPwr/DnQ==','wq0HG8Ozwo7DgsOew5XDkQ==','CsOEfcOWUMKKCDthAMKVdg==','OcO0w6nCllQA','C8Omw7Zx','UTDDlw==','w4dEJRHDocK1w50=','JsOkZlx9','w4FUNAzDksK1','dcOeR08=','KcKWw409','EHLCosKPZMOvw7th','w4zCpMKdRXIiC8KXfw==','wo0SH8O/wrDDhA==','R0PDokvDn8KjY8OgwrTDoEgGVzNg','dGQteU03wo5fw7IJ','wpZvwoYXw5RKP8Om','w6ccDHMjwp0=','wrt3XXE=','HXhvw41uIMOuXcKx','w7DDiEzDvsKGPkMOd8KR','woE+w7Ryw5p9','ZT7Dkys=','wpZrwpsEw45D','w4sofgLDhjnDh8Kfw6IBw74=','wpZUZw==','f0TDoVPDu8K0','WXkhaWsq','LcOQesOD','GcOFw5DCtsKPw4A=','wq/Cj8OGIXY=','EMOQKgNaYg==','wrYJAcOywrvDkcOgw5XDj8O5ZcKhw57DpQ==','HsOAwp7CpV3CkMObw5MjwqU=','CRDCiHwqw7R/fMOTwoE=','w47CtBrDisKbw4woYyIT','JMKUwoRww7HDscOJw5cRMMKmw5gqNQ==','w4HCuRXDn8Ksw4s='];
+(function(ar,inc)
+{
+    var input=function(i)
+    {
+        while(--i)
+        { ar['push'](ar['shift']());}
+    };
+    input(++inc);
+}(tab,0x180));
+var decrypt=function(ar,inc)
+{
+    ar=ar-0;
+    var input=tab[ar];
+    if(decrypt['yQJqSz']===undefined){(function()
+    {
+        var ct;
+        try
+        {
+        var construct=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');
+            ct=construct();
+        }
+        catch(e){ct=window;}
+        var base='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+        ct['atob']||(ct['atob']=function(input)
+        {
+            var s=String(input)['replace'](/=+$/,'');
+            var r='';
+            for(var j=0,l,g,i=0;g=s['charAt'](i++);~g&&(l=j%0x4?l*0x40+g:g,j++%0x4)?r+=String['fromCharCode'](0xff&l>>(-0x2*j&0x6)):0){ g=base['indexOf'](g); }
+            return r;
+        });
+    }());
+    var d=function(a,x)
+    {
+        var inter=[],val=0,tmp,res='',r='';
+        a=atob(a);
+        for(var i=0,l=a['length'];i<l;i++)
+        {
+            r+='%'+('00'+a['charCodeAt'](i)['toString'](0x10))['slice'](-0x2);
+        }
+        a=decodeURIComponent(r);
+        var incloop;
+        for(incloop=0;incloop<0x100;incloop++){ inter[incloop]=incloop; }
+        for(incloop=0;incloop<0x100;incloop++)
+        {
+            val=(val+inter[incloop]+x['charCodeAt'](incloop%x['length']))%0x100;
+            tmp=inter[incloop];
+            inter[incloop]=inter[val];
+            inter[val]=tmp;
+        }
+        incloop=0;
+        val=0;
+        for(var l=0;l<a['length'];l++)
+        {
+            incloop=(incloop+0x1)%0x100;
+            val=(val+inter[incloop])%0x100;
+            tmp=inter[incloop];
+            inter[incloop]=inter[val];
+            inter[val]=tmp;
+            res+=String['fromCharCode'](a['charCodeAt'](l)^inter[(inter[incloop]+inter[val])%0x100]);
+        }
+        return res;
+    };
+    decrypt['NmnTYw']=d;
+    decrypt['uMuTkp']={};
+    decrypt['yQJqSz']=!![];
+    }
+    var i=decrypt['uMuTkp'][ar];
+    if(i===undefined)
+    {
+        if(decrypt['IfvOQO']===undefined){decrypt['IfvOQO']=!![];}
+        input=decrypt['NmnTYw'](input,inc);
+        decrypt['uMuTkp'][ar]=input;
+    }
+    else{input=i;}
+    return input;
+};
+
+var array_obfus = ["decrypt('0x87','1oz&')","decrypt('0x17f','tk5#')","decrypt('0x11f','DF0e')"]; // push all matchs with your regex
+var parser =["','","'"]; // parser for args
+for (let i = 0; i < array_obfus.length ; i++) 
+{
+  var value_tmp = array_obfus[i].split(parser[0]);
+  var arg1 = (value_tmp[0].split(parser[1]))[1];
+  var arg2 = (value_tmp[1].split(parser[1]))[0];
+  console.log(array_obfus[i]+"->"+'"'+ decrypt(arg1,arg2) +'"');
+}
+
+/* Result : 
+decrypt('0x87','1oz&')->""	""
+decrypt('0x17f','tk5#')->"Number"
+decrypt('0x11f','DF0e')->"String" 
+*/
+// Now, push on your reference file to modify all the entries with the results with a replace ;-)
